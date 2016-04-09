f2c93a0ff9add80927c299f771425fcf1e4f0abc
chaitanyaPaikara <chaitanya1445@gmail.com>
2016-04-09 16:32:51 +0530

    Code

--- EXT_Interrupt/main.c
+++ EXT_Interrupt/main.c
@@ -4,44 +4,64 @@
 #include "stm32f4xx_syscfg.h"
 #include "misc.h"
 
-void EXTI15_10_IRQHandler(void)
+GPIO_InitTypeDef PIN;
+GPIO_InitTypeDef LED;
+EXTI_InitTypeDef EXT;
+NVIC_InitTypeDef VCT;
+
+volatile int i = 0;
+
+void EXTI0_IRQHandler(void)
 {
-	int i = 0;
 	GPIO_SetBits(GPIOD,GPIO_Pin_12);
 	for(i=0;i<500000;i++);
 	GPIO_ResetBits(GPIOD,GPIO_Pin_12);
 	for(i=0;i<500000;i++);
+
+	EXTI_ClearFlag(EXTI_Line0);
+
 }
 
 int main(void)
 {
 	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
-	GPIO_InitTypeDef PIN;
-	EXTI_InitTypeDef EXT;
-	NVIC_InitTypeDef VCT;
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_SYSCFG, ENABLE);
 
-	PIN.GPIO_Pin = GPIO_Pin_12;
+	PIN.GPIO_Pin = GPIO_Pin_0;
 	PIN.GPIO_Mode = GPIO_Mode_IN;
-	PIN.GPIO_Speed = GPIO_Speed_25MHz;
-	PIN.GPIO_PuPd = GPIO_PuPd_UP;
+	PIN.GPIO_PuPd = GPIO_PuPd_NOPULL;
 
-	GPIO_Init(GPIOD,&PIN);
+	GPIO_Init(GPIOA,&PIN);
 
-	EXT.EXTI_Line = EXTI_Line12;
+	EXT.EXTI_Line = EXTI_Line0;
 	EXT.EXTI_LineCmd = ENABLE;
 	EXT.EXTI_Mode = EXTI_Mode_Interrupt;
-	EXT.EXTI_Trigger = EXTI_Trigger_Falling;
+	EXT.EXTI_Trigger = EXTI_Trigger_Rising;
 
-	SYSCFG_EXTILineConfig(EXTI_PortSourceGPIOD, EXTI_PinSource12);
+	SYSCFG_EXTILineConfig(EXTI_PortSourceGPIOA, EXTI_PinSource0);
 
 	EXTI_Init(&EXT);
 
-	VCT.NVIC_IRQChannel = EXTI15_10_IRQn;
+	VCT.NVIC_IRQChannel = EXTI0_IRQn;
 	VCT.NVIC_IRQChannelPreemptionPriority = 0x00;
 	VCT.NVIC_IRQChannelSubPriority = 0x00;
 	VCT.NVIC_IRQChannelCmd = ENABLE;
 
 	NVIC_Init(&VCT);
+
+	LED.GPIO_Pin = GPIO_Pin_12;
+	LED.GPIO_Mode = GPIO_Mode_OUT;
+	LED.GPIO_OType = GPIO_OType_PP;
+	LED.GPIO_Speed = GPIO_Speed_25MHz;
+	LED.GPIO_PuPd = GPIO_PuPd_NOPULL;
+
+	GPIO_Init(GPIOD,&LED);
+
+	GPIO_SetBits(GPIOD,GPIO_Pin_12);
+	for(i=0;i<500000;i++);
+	GPIO_ResetBits(GPIOD,GPIO_Pin_12);
+	for(i=0;i<500000;i++);
 
     while(1)
     {
